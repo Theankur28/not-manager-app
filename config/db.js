2178e3ed42447f961aaead685a2dc3bb5bbadd12
Theankur28 <ankurpurohit6056@gmail.com>
2023-08-14 15:22:17 +0530

    done some modification

--- config/db.js
+++ config/db.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
-const URI = `mongodb://0.0.0.0:27017/-note-manager-app`
+const MONGO_USER = "ankur_028";
+const MONGO_PASS = "ankurpurohit@28";
+const MONGO_CLUSTER = "cluster0.v6df8xl.mongodb.net";
+const MONGO_URI = `mongodb+srv://${encodeURIComponent(MONGO_USER)}:${encodeURIComponent(MONGO_PASS)}@${MONGO_CLUSTER}/doctor-appointment-app`;
 
-
-const connectToMongo = async () => {
-    const res = await mongoose.connect(URI);
-    try{
-        if(res){
-            console.log("Database Connected Successfully!");
-        }
-    } catch(error){
-        console.log(error);
-    }
-}
+const connectDB = async () => {
+   try {
+      const res = await mongoose.connect(MONGO_URI);
+      if (res) {
+         console.log("Database Connected Successfully");
+      }
+   } catch (error) {
+      console.log(error);
+   }
+};
 
 export default connectToMongo;
