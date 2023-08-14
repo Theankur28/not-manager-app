262ab82481bee7f338a27436b0a09950e3857f35
Theankur28 <ankurpurohit6056@gmail.com>
2023-08-14 15:51:13 +0530

    Done some modifications

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGO_USER = "ankur_028";
 const MONGO_PASS = "ankurpurohit@28";
 const MONGO_CLUSTER = "cluster0.v6df8xl.mongodb.net";
-const MONGO_URI = `mongodb+srv://${encodeURIComponent(MONGO_USER)}:${encodeURIComponent(MONGO_PASS)}@${MONGO_CLUSTER}/doctor-appointment-app`;
+const MONGO_URI = `mongodb+srv://${encodeURIComponent(MONGO_USER)}:${encodeURIComponent(MONGO_PASS)}@${MONGO_CLUSTER}/note-taking-app`;
 
 const connectToMongo = async () => {
    try {
